5c4a78be78d748d3c77f95c978837625c039ef19
mahabubr <mahabub.r753@gmail.com>
2022-11-24 19:31:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+require('colors');
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const port = process.env.PORT || 5000
+
+// Middle Ware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('O Watch Server Running')
+})
+
+// app.get("*", (req, res) => {
+//     res.json({ message: "You Are in Wrong Way" })
+// })
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.vlhy1ml.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        // Mongodb Collections
+        const watchCategoryCollection = client.db('oWatchCheck').collection('WatchCategory')
+
+
+        app.get('/watch-category', async (req, res) => {
+            const query = {}
+            const result = await watchCategoryCollection.find(query).toArray()
+            res.send(result)
+        })
+
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+run()
+
+app.listen(port, () => {
+    console.log(`Server Is Running in PORT ${port}`.bgMagenta);
+})
